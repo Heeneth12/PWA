b1871d264170344503658e3da71fdbcc49c874e4
Heeneth-sai <heeneth.sai@dragarwal.com>
2024-08-04 11:20:29 +0530

    added new  pwa config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environment/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+if ('serviceWorker' in navigator && environment.production) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then(
+      (registration) => {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        );
+      },
+      (err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      }
+    );
+  });
+}
