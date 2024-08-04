497ed7ebeb48762c8c2ed69ed06eeabba1570a28
Heeneth-sai <heeneth.sai@dragarwal.com>
2024-08-04 10:29:35 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { PwaComponent } from './pwa/pwa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: PwaComponent }];
---
+++ src/app/pwa/pwa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pwa',
+  standalone: true,
+  imports: [],
+  templateUrl: './pwa.component.html',
+  styleUrl: './pwa.component.css'
+})
+export class PwaComponent {
+
+}
