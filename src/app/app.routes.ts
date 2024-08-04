d8b3939636e07777768558991ca536ec2f66ce6c
Heeneth-sai <heeneth.sai@dragarwal.com>
2024-08-04 10:48:12 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { PwaComponent } from './pwa/pwa.component';
 
-export const routes: Routes = [
-  // {path : '' , component : PwaComponent}
-];
+export const routes: Routes = [{ path: 'test', component: PwaComponent }];
